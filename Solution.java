60e015dad67a0e8aed4d1259162d95efbb64df62
NITHISH REDDY PUTTA <124814443+NITHISH5544@users.noreply.github.com>
2024-09-06 09:19:47 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public static void main(String[] args) {
+        String roman = "MCMXCIV";
+        int result = romanToInt(roman);
+        System.out.println("The integer value of " + roman + " is: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        char[] x = new char[15];
+        int sum = 0;
+        int l = s.length();
+        if (l > 0 && l < 15) {
+            s.getChars(0, l, x, 0);
+        
+
+        for (int i = 0; i < l; i++) {
+            switch (x[i]) {
+                case 'I':
+                    if (i + 1 < l && (x[i + 1] == 'V' || x[i + 1] == 'X')) {
+                        sum -= 1;
+                    } else {
+                        sum += 1;
+                    }
+                    break;
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'X':
+                    if (i + 1 < l && (x[i + 1] == 'L' || x[i + 1] == 'C')) {
+                        sum -= 10;
+                    } else {
+                        sum += 10;
+                    }
+                    break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if (i + 1 < l && (x[i + 1] == 'D' || x[i + 1] == 'M')) {
+                        sum -= 100;
+                    } else {
+                        sum += 100;
+                    }
+                    break;
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000;
+                    break;
+                
+            }
+        }}
+        return sum;
+    
+}}
